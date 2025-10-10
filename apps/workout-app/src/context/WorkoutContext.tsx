708910c6c3f834ddecd8cff503275012d73aaab3
Arul Rajeev <85393585+Arul-R@users.noreply.github.com>
2025-10-10 13:04:32 +0530

    Update WorkoutContext.tsx
    added mock workouts

--- apps/workout-app/src/context/WorkoutContext.tsx
+++ apps/workout-app/src/context/WorkoutContext.tsx
@@ -8,7 +8,104 @@ type WorkoutContextType = {
 };
 
 const initialWorkouts: WorkoutType[] = [
-  // Paste all 7 initial workouts from your Expo code here
+  {
+    id: 'day1',
+    date: '9/12/2025',
+    exercises: [
+      {
+        name: 'bench press',
+        sets: [{ reps: '10', rir: '2', weight: '40' }],
+      },
+      {
+        name: 'squat',
+        sets: [{ reps: '12', rir: '2', weight: '60' }],
+      },
+    ],
+  },
+  {
+    id: 'day2',
+    date: '9/13/2025',
+    exercises: [
+      {
+        name: 'bench press',
+        sets: [{ reps: '10', rir: '2', weight: '42.5' }],
+      },
+      {
+        name: 'lat pulldown',
+        sets: [{ reps: '12', rir: '2', weight: '40' }],
+      },
+    ],
+  },
+  {
+    id: 'day3',
+    date: '9/14/2025',
+    exercises: [
+      {
+        name: 'bench press',
+        sets: [{ reps: '10', rir: '2', weight: '45' }],
+      },
+      {
+        name: 'shoulder press',
+        sets: [{ reps: '12', rir: '2', weight: '25' }],
+      },
+    ],
+  },
+  {
+    id: 'day4',
+    date: '9/15/2025',
+    exercises: [
+      {
+        name: 'bench press',
+        sets: [{ reps: '10', rir: '2', weight: '47.5' }],
+      },
+      {
+        name: 'bicep curl',
+        sets: [{ reps: '12', rir: '2', weight: '15' }],
+      },
+    ],
+  },
+  {
+    id: 'day5',
+    date: '9/16/2025',
+    exercises: [
+      {
+        name: 'bench press',
+        sets: [{ reps: '10', rir: '2', weight: '50' }],
+      },
+      {
+        name: 'tricep pushdown',
+        sets: [{ reps: '15', rir: '2', weight: '25' }],
+      },
+    ],
+  },
+  {
+    id: 'day6',
+    date: '9/17/2025',
+    exercises: [
+      {
+        name: 'bench press',
+        sets: [{ reps: '10', rir: '2', weight: '52.5' }],
+      },
+      {
+        name: 'romanian deadlift',
+        sets: [{ reps: '12', rir: '2', weight: '70' }],
+      },
+    ],
+  },
+  {
+    id: 'day7',
+    date: '9/18/2025',
+    exercises: [
+      {
+        name: 'bench press',
+        sets: [{ reps: '10', rir: '2', weight: '55' }],
+      },
+      {
+        name: 'pull ups',
+        sets: [{ reps: '8', rir: '1', weight: 'bodyweight' }],
+      },
+    ],
+  },
 ];
 
 const WorkoutContext = createContext<WorkoutContextType | undefined>(undefined);
